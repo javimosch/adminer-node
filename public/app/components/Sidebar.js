8b5a278203197c870e21c6e0b5341f28c3193770
Jarancibia <arancibiajav@gmail.com>
2026-02-26 18:02:26 +0100

    feat: Add "Home" menu item to Sidebar

--- public/app/components/Sidebar.js
+++ public/app/components/Sidebar.js
@@ -71,6 +71,10 @@ export default {
       <ul class="menu menu-sm w-full">
         <li><a @click.prevent="navigate('/home')" href="#/home" class="text-sm font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/></svg>
+          Home
+        </a></li>
+        <li><a @click.prevent="navigate('/databases')" href="#/databases" class="text-sm font-medium">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2"/></svg>
           Server Info
         </a></li>
         <li v-if="store.conn?.driver !== 'sqlite'">
